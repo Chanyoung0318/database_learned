34e635712c854c1c3bb85c9f5d6f50f9951ccc92
Chanyoung0318 <120690625+Chanyoung0318@users.noreply.github.com>
2023-07-28 16:16:54 +0900

    Update hr.sql

--- hr.sql
+++ hr.sql
@@ -53,3 +53,84 @@ where commission_pct is not null and salary >= 10000;
 select first_name || last_name
 from employees;
 
+----------------------------------------------------------------
+
+
+
+select *
+from employees
+where salary > 8000 and salary < 10000;
+
+select *
+from employees
+where salary >= 7000 or salary <= 10000;
+
+select *
+from employees
+where department_id != 80;
+
+select *
+from employees
+where commission_pct is not null and hire_date < '2008:01:01';
+
+select *
+from regions
+where region_id = 1 or region_id = 3 or region_id = 4;
+
+
+select nvl(commission_pct, 0)
+from employees;
+
+select commission_pct
+from employees;
+
+select nvl2(commission_pct, 'not null', 'null')
+from employees;
+
+select decode(commission_pct, null, 0 ,commission_pct)
+from employees;
+
+select nvl2(commission_pct, 'yes', 'no')
+from employees;
+
+
+select employee_id as no, first_name as name, salary as salary, department_name as d_name, location_id as location
+from departments, employees
+where employees.salary <= 2000;
+
+
+
+select *
+from employees
+where commission_pct in (0.1, 0.25, 0.2, 0.3);
+
+select *
+from employees
+where commission_pct not in(0.1, 0.25, 0.2, 0.3) or commission_pct is null;
+
+select *
+from employees
+where department_id in(50, 80);
+
+select *
+from employees
+where phone_number like '011%';
+
+select *
+from employees
+where email like '__E%';
+
+select COMMISSION_PCT
+from employees
+order by COMMISSION_PCT asc;
+
+select COMMISSION_PCT
+from employees
+order by COMMISSION_PCT desc;
+
+
+create table c2 as select * from employees;
+
+select *
+from c2;
+
