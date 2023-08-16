32583e1035d683c781148dcf373d889359a84c5b
Chanyoung0318 <120690625+Chanyoung0318@users.noreply.github.com>
2023-08-16 16:46:24 +0900

    Add files via upload

---
+++ JavaStart01.java
@@ -0,0 +1,313 @@
+package com.human.ex;
+
+public class JavaStart01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("HelloWorld~");
+		
+		
+		//System.out.println("안녕 자바");
+		
+		//System.out.println("이찬영\ncksdud6788@naver.com\n010-9650-6778");
+		
+		/* System.out.println("*****\n*****\n*****\n*****\n*****\n");
+		System.out.println("*\n**\n***\n****\n*****");
+		System.out.println("\n******\n****\n***\n**\n*"); */
+		
+		/*System.out.println("***********************");
+		System.out.println("*                     *");
+		System.out.println("*        성적표         *");
+		System.out.println("***********************");
+		System.out.println("*  국어     50         *");
+		System.out.println("***********************");
+		System.out.println("*  영어     50         *");
+		System.out.println("***********************");
+		System.out.println("*  수학     50         *");
+		System.out.println("***********************");
+		System.out.println("*컴퓨터학과 홍길동         *");
+		System.out.println("***********************");*/
+		
+		/*System.out.println("일\t월\t화\t수\t목\t금\t토");
+		System.out.println("1\t2\t3\t4\t5\t6\t7");
+		System.out.println("8\t9\t10\t11\t12\t13\t14");
+		System.out.println("15\t16\t17\t18\t19\t20\t21");
+		System.out.println("22\t23\t24\t25\t26\t27\t28");
+		System.out.println("29\t30\t31");*/
+		
+		/*System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");*/
+		
+		/*System.out.print("1");
+		System.out.print("2");
+		System.out.print("3");*/
+		
+		/*System.out.print("hello");
+		System.out.println(" world");
+		System.out.println(" world");
+		System.out.print("Hello");*/
+		
+		/*System.out.print("12");
+		System.out.println("34");
+		System.out.println("56");*/
+		
+		/*System.out.println("hello");
+		System.out.print("java");*/
+		
+		/*System.out.print(" ");
+		System.out.print(" ");
+		System.out.print("1");
+		System.out.println("");
+		System.out.print(" ");
+		System.out.print("12");
+		System.out.println("");
+		System.out.print("123");*/
+		
+		/*System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");*/
+		
+		/*System.out.print("*");
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print(" ");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");*/
+		
+		/*System.out.print(" ");
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print(" ");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");*/
+		
+		/*System.out.print(" ");
+		System.out.print(" ");
+		System.out.print("*");
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.println("");
+		System.out.print(" ");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print(" ");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print(" ");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print(" ");
+		System.out.println("");
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.print("*");
+		System.out.print(" ");
+		System.out.print(" ");*/
+		
+		//System.out.println(100);
+		
+		//System.out.println(20+30);
+		
+		//System.out.println("20+30");
+		
+		//System.out.println(15.4);
+		
+		//System.out.println("15.4");
+		
+		//System.out.println("10"+20+20);
+		
+		//System.out.println(10+10+"20");
+		
+		//System.out.println(2+6*(4*2));
+		
+		//System.out.println("2*(4+2+2)+(4+1*2)");
+		
+		//System.out.println("(5+3)");
+		
+		//System.out.println("7+3="+(7+3));
+		
+		//System.out.println("7+3="+7+3);
+		
+		//System.out.println(3.14E-4);
+		
+		
+		//int a=10;
+		//System.out.println(a);
+		
+		//int b=30;
+		//System.out.println(b);
+		
+		//System.out.println(b+10);
+		
+		//int c=30;
+		//System.out.println(c+c);
+		
+		//int hello=50;
+		//System.out.println(hello+30);
+		
+		//int d=30;
+		//d=60;
+		//System.out.println(d);
+		
+		/*int i1=22;
+		int i2=11;
+		int i3=33;
+		System.out.println(i1+i2+i3);*/
+		
+		/*int world=5;
+		System.out.println(world);
+		int a=6;
+		System.out.println(world+a);*/
+		
+		
+		//int world=5;
+		//System.out.println(world+5);
+		
+		/*int world=5;
+		System.out.println("world"+5);*/
+		
+		//int world=5;
+		//System.out.println("world"+world);
+		
+		//int a=5;
+		//int b=6;
+		//System.out.println("a+b="+a+b);
+		
+		/*int a=5;
+		int b=6;
+		System.out.println("a+b="+(a+b));*/
+		
+		/*int a=5;
+		System.out.println(a);
+		a=3;
+		System.out.println(a);*/
+		
+		/*int a=5;
+		System.out.println(a);
+		a=6;
+		System.out.println(a);
+		int b=5;
+		System.out.println(a+b);*/
+		
+		/*int a=5;
+		a=6;
+		a=7;
+		a=8;
+		System.out.println(a);*/
+		
+		/*int a=5;
+		double b=5.1;
+		String st="hello";
+		System.out.println(a+b+st);
+		System.out.println(st+b+a);
+		System.out.println("a+b="+(a+b)+st);*/
+		
+		/*int a=5;
+		a=a+2;
+		System.out.println(a);*/
+		
+		/*int sum=0;
+		sum=sum+1;
+		System.out.println(sum);
+		sum=sum+2;
+		System.out.println(sum);
+		sum=sum+3;
+		System.out.println(sum);
+		int a=5;
+		sum=a+5;
+		System.out.println(sum);
+		a=7;
+		sum=sum+a;
+		System.out.println(sum);*/
+		
+		
+		/*String name="홍길동";
+		int age=29;
+		double height=170.1;
+		System.out.print("이름: "+name+"\n나이: "+age+"\n키: "+height);*/
+		
+		/*int a=5;
+		int b=10;
+		System.out.print(a+b);*/
+		
+		
+		
+		
+		//System.out.print(100);
+		
+		//System.out.print(20+30);
+		
+		//System.out.println("20+30");
+		
+		//System.out.print(15.4);
+		
+		//System.out.print("15.4");
+		
+		//System.out.print(10+10+"20");
+		
+		//System.out.println(2+6*(4*2));
+	
+		//2*(4+3+2)+(4+1*2);
+
+		//System.out.print(2*(4+3+2)+(4+1*2));
+		
+		//System.out.println("(5+3)");
+		
+		//System.out.println("7+3="+(7+3));
+		
+		//System.out.println("7+3="+7+3);
+		
+	}
+
+}
