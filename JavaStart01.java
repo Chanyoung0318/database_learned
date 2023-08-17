4685118c5ef4d4ee9d08a470fcc40ca9c15498ab
Chanyoung0318 <120690625+Chanyoung0318@users.noreply.github.com>
2023-08-17 16:59:31 +0900

    Add files via upload

--- JavaStart01.java
+++ JavaStart01.java
@@ -284,29 +284,213 @@ public class JavaStart01 {
 		
 		
 		
-		//System.out.print(100);
+		/*System.out.println(100);
+		System.out.println(20+30);
+		System.out.println("20+30");
+		System.out.println(15.4);
+		System.out.println("15.4");
+		System.out.println(10+10+"20");
+		System.out.println(2+6*(4*2));
+		System.out.println(2*(4+3+2)+(4+1*2));
+		System.out.println("(5+3)");
+		System.out.println("7+3="+(7+3));
+		System.out.println("7+3="+7+3);*/
 		
-		//System.out.print(20+30);
+		/*boolean b=true;
+		int i=1;
+		short s=2;
+		char c='a';
+		float f= 1.234f;*/
 		
-		//System.out.println("20+30");
 		
-		//System.out.print(15.4);
 		
-		//System.out.print("15.4");
+		/*short s2= (short)(s+c);
+		short s3= (short)(s+s);
+		char c2 = (char)(c+s);
+		int i2 = (int)(c+s);
+		int i3 = (b ? 1 : 0) + i;
 		
-		//System.out.print(10+10+"20");
+		System.out.println(s2);
+		System.out.println(s3);
+		System.out.println(c2);
+		System.out.println(i2);
+		System.out.println(i3);*/
 		
-		//System.out.println(2+6*(4*2));
-	
-		//2*(4+3+2)+(4+1*2);
+		/*String name = "홍길동";
+		int age=5;
+		
+		System.out.print("이름은 "+name+" 나이는 "+age+" 입니다");*/
+		
+		
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		
+		/*String str1 = sc.nextLine();
+		String str2 = sc.nextLine();
+		
+		System.out.print(str1+str2);*/
+		
+		/*System.out.print("세로 길이를 입력하시오 : ");
+		int height = Integer.parseInt(sc.nextLine());
+		
+		System.out.print("가로 길이를 입력하시오 : ");
+		int width = Integer.parseInt(sc.nextLine());
+		
+		int result = height * width;
+		System.out.print("사각형의 넓이 : "+result);*/
+		
+		/*System.out.print("높이 길이를 입력하시오 : ");
+		int height = Integer.parseInt(sc.nextLine());
+		
+		System.out.print("가로 길이를 입력하시오 : ");
+		int x = Integer.parseInt(sc.nextLine());
+		
+		System.out.print("세로 길이를 입력하시오 : ");
+		int y = Integer.parseInt(sc.nextLine());
+		
+		int result = x*y*height;
+		System.out.print("사각기둥의 넓이 : "+result);*/
+		
+		
+		/*System.out.print("cm 입력: ");
+		int cm = Integer.parseInt(sc.nextLine());
+		
+		int meter = cm/100;
+		System.out.println(cm+"cm은 "+meter+"m");*/
+		
+		
+		/*System.out.print("잔디의 가격: ");
+		int grass = Integer.parseInt(sc.nextLine());
+		
+		System.out.print("사다리꼴 윗변 길이: ");
+		int y = Integer.parseInt(sc.nextLine());
+		
+		System.out.print("사다리꼴 아래변 길이: ");
+		int x = Integer.parseInt(sc.nextLine());
+		
+		System.out.print("사다리꼴 높이 길이: ");
+		int hei = Integer.parseInt(sc.nextLine());
+		
+		int result = grass*((y*x)*hei/2);
+		
+		System.out.println("비용 : "+result+" 원");*/
+		
+		
+		/*System.out.print("이름을 입력하세요 : ");
+		String name = sc.nextLine();
+		
+		System.out.print("나이를 입력하세요: ");
+		int age = Integer.parseInt(sc.nextLine());
+		
+		System.out.print("키를 입력하세요 : ");
+		double height = Double.parseDouble(sc.nextLine());
+		
+		System.out.println("이름 : "+name+ "\n나이 : "+age+"세\n키 : "+height+"cm");*/
+		
+		
+		/*System.out.print("첫번째 수를 입력하시오: ");
+		double num1 = Double.parseDouble(sc.nextLine());
 
-		//System.out.print(2*(4+3+2)+(4+1*2));
+		System.out.print("두번째 수를 입력하시오: ");
+		double num2 = Double.parseDouble(sc.nextLine());
 		
-		//System.out.println("(5+3)");
+		double result = num1 - num2;
 		
-		//System.out.println("7+3="+(7+3));
+		System.out.println("두 수의 차이 결과값 : "+result);*/
 		
-		//System.out.println("7+3="+7+3);
+		
+		/*System.out.print("국어: ");
+		int kor = Integer.parseInt(sc.nextLine());
+		
+		System.out.print("영어: ");
+		int eng = Integer.parseInt(sc.nextLine());
+		
+		System.out.print("수학: ");
+		int mat = Integer.parseInt(sc.nextLine());
+		
+		int result = (kor+eng+mat)/3;
+		
+		System.out.print("평균 값 : "+ result);*/
+		
+		
+		/*System.out.print("키(m) 입력: ");
+		double height = Double.parseDouble(sc.nextLine());
+		double result = Math.round(height * 100);
+		System.out.println("키(cm) : "+result+"cm");*/
+		
+		/*System.out.print("상자의 가로 : ");
+		int x = Integer.parseInt(sc.nextLine());
+		
+		System.out.print("상자의 세로: ");
+		int y = Integer.parseInt(sc.nextLine());
+		
+		System.out.print("상자의 높이: ");
+		int height = Integer.parseInt(sc.nextLine());
+		
+		int result = x*y*height;
+		
+		System.out.print("상자의 부피 : "+result);*/
+		
+		/*System.out.print("연필 박스 구매 개수: ");
+		int box = Integer.parseInt(sc.nextLine());
+		
+		System.out.print("연필 자루 개수: ");
+		int pen = Integer.parseInt(sc.nextLine());
+		
+		int result = (box*12+pen)*1000;
+		
+		System.out.print("지불해야 할 값 : "+ result + "원");*/
+		
+		/*int age = 156;
+		String name = "hong";
+		double height = 175.3;
+		
+		String str1 = String.format("이름 : %s\n키 : %f\n나이 : %d", name, height, age);
+		System.out.print(str1);*/
+		
+		
+		/*System.out.print("첫번째 정수 입력 :");
+		int num1 = Integer.parseInt(sc.nextLine());
+		
+
+		System.out.print("두번째 정수 입력 :");
+		int num2 = Integer.parseInt(sc.nextLine());
+		
+		int result1 = num1 / num2;
+		int result2 = num1 % num2;
+		System.out.printf("몫은 %d 이고, 나머지는 %d이다", result1, result2);*/
+		
+		
+		/*System.out.print("숫자 입력 :");
+		int num = Integer.parseInt(sc.nextLine());
+		
+		int num1 = num / 100;
+		
+		int num2 = (num % 100) / 10;
+		
+		int num3 = num % 10;
+		System.out.printf("첫번째 자리 : %d, 두번째 자리 : %d, 세번째 자리 : %d", num1, num2, num3);*/
+		
+		
+		/*System.out.print("첫번째 점 x 입력 :");
+		int fx = Integer.parseInt(sc.nextLine());
+		
+		System.out.print("첫번째 점 y 입력 :");
+		int fy = Integer.parseInt(sc.nextLine());
+		
+		
+		System.out.print("두번째 점 x 입력 :");
+		int sx = Integer.parseInt(sc.nextLine());
+
+		System.out.print("두번째 점 y 입력 :");
+		int sy = Integer.parseInt(sc.nextLine());
+		
+		int re = (sx - fx)*(sx - fx) + (sy - fy)*(sy - fy);
+		
+		double result = Math.sqrt(re);
+		
+		System.out.printf("두 점 사이의 거리: %f", result);*/
+		
+		
 		
 	}
 
